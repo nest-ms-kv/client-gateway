e6ed635f17e5dd753de8b2c0708aee8e3f9c07bc
Keneth Vasquez <kfvasque@pichincha.com>
2024-08-19 09:29:01 -0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
+  console.log('Hola mundo - primer cambio')
   logger.log(`Gateway is running on: ${envs.port}`);
 }
 bootstrap();
