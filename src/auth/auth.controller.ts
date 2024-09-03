8da8d453fbbe343caa0bb79250fd39bf08ae7b57
Keneth Vasquez <kfvasque@pichincha.com>
2024-09-03 12:50:24 -0600

    add ECR

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto);
+  }
+
+  @Get('verify')
+  verifyUser() {
+    return this.client.send('auth.verify.user', {});
+  }
+}
